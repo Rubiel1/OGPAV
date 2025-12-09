3870a38b537a3a444988565cf6d1fb2d661e9a08
Eric Rubiel <eric.rubiel@u.northwestern.edu>
2025-12-09 09:10:12 +0900

    Update gpav_test.py
    Added test to verify some terms are not touched

--- tests/gpav_test.py
+++ tests/gpav_test.py
@@ -19,7 +19,7 @@ class TestClass:
         v, _, _ = gpav_seg(Y_aligned, G, topo_order=T, verbose=True)
         assert set(u) == set([1.0,2.0,3.0,4.0]), "error of GPAV_op when the input is the trivial line already ordered "
         assert set(v) == set([1.0,2.0,3.0,4.0]), "error of GPAV_seg when the input is the trivial line already ordered "
-    def test_c_op(self):
+    def test_closed_op(self):
         # a<b<c<d<e, a<f<e
         # Example 1: Small Q with 4 components R_i
         poset = hasse.PoSet.from_chains([0, 1, 2], [0, 3, 2])
@@ -41,3 +41,24 @@ class TestClass:
             verbose=True,
         )
         assert set(u) == {np.float64(10.0), np.float64(11.0), np.float64(12.0)}, "error of factorized_gpav_fast_parallel"
+    def test_position_op(self):
+        # a<b<c, compose on b by {x,y,z} < {X,Y,Z}
+        # Example 1: Small Q with 6 components R_i
+        poset = hasse.PoSet.from_chains([0,1,2])
+        R_subposets = [
+            hasse.PoSet.from_chains([0],),
+            hasse.PoSet.from_chains([0,1],[2,3],[4,5],[0,3],[2,1],[2,5],[4,3]),
+            hasse.PoSet.from_chains([0]),
+        ]
+        Y = [1,7,5,3,6,9,10,4]
+        Y_map = {i: float(y) for i, y in enumerate(Y)}
+        u = factorized_gpav_fast_parallel(
+            Q=poset,
+            R_subposets=R_subposets,
+            A=Y_map,
+            use_lowerY_first=True,
+            use_lowerY_blocks=True,
+            inputs_are_reduced=False,
+            verbose=True,
+        )
+        assert u_[2] == Y[2], "error on the implementation of factorized_gpav_fast_parallel, it is modifying nodes that are supposed to be untouched"
