86303afd4aa78eb92c2143c64a3c907096dc2303
Eric Rubiel <eric.rubiel@u.northwestern.edu>
2026-03-24 11:49:35 +0900

    Change cache_dir to use TemporaryDirectory

--- utils/geometric_sb_dataset.py
+++ utils/geometric_sb_dataset.py
@@ -462,7 +462,7 @@ def generate_dataset_lazy(
     Global X and P_hasse are NOT built to save memory.
     """
     if cache_dir is None:
-        cache_dir = tempfile.mkdtemp(prefix="ogpav_cache_")
+        cache_dir = tempfile.TemporaryDirectory(prefix="ogpav_cache_")
     else:
         os.makedirs(cache_dir, exist_ok=True)
 
